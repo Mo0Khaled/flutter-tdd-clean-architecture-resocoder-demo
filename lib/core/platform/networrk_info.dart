dd58cf67c89355946cdd0b4eaea7d0da3a4c8ac2
mo <mohamedkx77@gmail.com>
2022-02-19 23:40:17 +0200

    network info done

--- lib/core/platform/networrk_info.dart
+++ lib/core/platform/networrk_info.dart
@@ -1,3 +1,13 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+class NetworkInfoImpl implements NetworkInfo {
+  final DataConnectionChecker dataConnectionChecker;
+  NetworkInfoImpl(this.dataConnectionChecker);
+
+  @override
+  Future<bool> get isConnected => dataConnectionChecker.hasConnection;
+
 }
---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,35 @@
+import 'package:clean_arc/core/platform/networrk_info.dart';
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../features/number_trivia/data/repositories/number_trivia_repository_impl_test.mocks.dart';
+import 'network_info_test.mocks.dart';
+
+@GenerateMocks([DataConnectionChecker])
+void main() {
+  late NetworkInfoImpl networkInfoImpl;
+  late MockDataConnectionChecker mockDataConnectionChecker;
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+    group('isConnected', () {
+    test(
+      'should forward the call to DataConnectionChecker has connection',
+          () async {
+        // arrange
+        final tHasConnectionFuture = Future.value(true);
+        when(mockDataConnectionChecker.hasConnection).thenAnswer((_) => tHasConnectionFuture);
+        // act
+        final result = networkInfoImpl.isConnected;
+        // assert
+        verify(mockDataConnectionChecker.hasConnection);
+        expect(result, tHasConnectionFuture);
+      },
+    );
+  });
+
+}
