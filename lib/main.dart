a2f5ab4040e65f75b4662f24f9729c32ff89c788
mo <mohamedkx77@gmail.com>
2022-02-28 22:42:22 +0200

    finished UI and all done ✨

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,26 @@
+import 'package:clean_arc/features/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'package:flutter/material.dart';
 import 'injection_container.dart' as di;
-Future<void> main() async {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-// dart run build_runner build
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Number Trivia',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.red.shade800,
+        appBarTheme: AppBarTheme(color: Colors.red.shade800),
+        accentColor: Colors.red.shade600
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const NumberTriviaPage(),
     );
   }
 }
